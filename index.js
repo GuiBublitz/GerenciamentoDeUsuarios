2ec5702a7892a2867eda533cedf95d1fce7e6952
Guilherme Bublitz <gui.e.bublitz@gmail.com>
2021-12-27 19:47:38 -0200

    Trabalhando com funções

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 var fields =  document.querySelectorAll("#form-user-create [name]");
 var user = {};
+
+function addUser(userDate){
+    console.log(userDate);
+    let tr = document.createElement('tr');
+    tr.innerHTML = 
+    `
+    <tr>
+        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+        <td>${userDate.name}</td>
+        <td>${userDate.email}</td>
+        <td>${userDate.admin}</td>
+        <td>${userDate.birth}</td>
+        <td>
+            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+        </td>
+    </tr>
+    `
+    document.getElementById('table-users').appendChild(tr);
+}
+
 document.getElementById('form-user-create').addEventListener('submit',(e)=>{
     e.preventDefault();
     fields.forEach((field,index) => {
@@ -11,5 +32,5 @@ document.getElementById('form-user-create').addEventListener('submit',(e)=>{
             user[field.name] = field.value;
         }
     });
-    console.log(user);
+    addUser(user);
 });
