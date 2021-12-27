3f8a78d1aced20bf575321af5d8e86f4953e7f21
Guilherme Bublitz <gui.e.bublitz@gmail.com>
2021-12-27 18:57:31 -0200

    Trabalhando com Eventos no JavaScript

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 var fields =  document.querySelectorAll("#form-user-create [name]");
 var user = {};
-
-fields.forEach((field,index) => {
-    if(field.name == 'gender') {
-        if(field.checked){
-            user[field.name] = field.value;   
+document.getElementById('form-user-create').addEventListener('submit',(e)=>{
+    e.preventDefault();
+    fields.forEach((field,index) => {
+        if(field.name == 'gender') {
+            if(field.checked){
+                user[field.name] = field.value;   
+            }
+        } else {
+            user[field.name] = field.value;
         }
-    } else {
-        user[field.name] = field.value;
-    }
+    });
+    console.log(user);
 });
-console.log(user);
-
