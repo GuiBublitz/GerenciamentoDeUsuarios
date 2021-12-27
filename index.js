194b1b43cdebd12ec29988e7b9eeef75504ab3f0
Guilherme Bublitz <gui.e.bublitz@gmail.com>
2021-12-27 13:41:53 -0200

    Variáveis

---
+++ index.js
@@ -0,0 +1,7 @@
+let inputName     = document.querySelector('#exampleInputName');
+let maleRadio     = document.querySelector('#exampleInputGenderM');
+let femaleRadio   = document.querySelector('#exampleInputGenderF');
+let inputBirth    = document.querySelector('#exampleInputBirth');
+let selectCountry = document.querySelector('#exampleInputCountry');
+let inputEmail    = document.querySelector('#exampleInputEmail1');
+let inputPassword = document.querySelector('#exampleInputPassword1');
