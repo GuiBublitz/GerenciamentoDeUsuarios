bded6080942c57f8f0915cf22ed3fe047fbd04de
Guilherme Bublitz <gui.e.bublitz@gmail.com>
2021-12-28 23:04:19 -0200

    Trabalhando com Promise no JavaScript

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -11,24 +11,34 @@ class UserController {
         this.formEl.addEventListener('submit', (e)=>{
             e.preventDefault();
             let values = this.getValues();
-            this.getPhoto((content) => {
-                values.photo = content;
-                this.addUser(values);
-            });
+            this.getPhoto().then(
+                (content)=>{
+                    values.photo = content;
+                    this.addUser(values);
+                },
+                (e)=>{
+                    console.error(e);   
+                }
+            );                      
         });   
     }
-    getPhoto(callback){
-        let fileReader = new FileReader();
-        let elements = [...this.formEl.elements].filter( (item) => {
-            if (item.name === 'photo'){
-                return item;
-            }
-        });
-        let file = elements[0].files[0];
-        fileReader.onload = () => {
-            callback(fileReader.result);
-        };
-        fileReader.readAsDataURL(file);
+    getPhoto(){
+        return new Promise((resolve, reject) => {
+            let fileReader = new FileReader();
+            let elements = [...this.formEl.elements].filter( (item) => {
+                if (item.name === 'photo'){
+                    return item;
+                }
+            });
+            let file = elements[0].files[0];
+            fileReader.onload = () => {
+                resolve(fileReader.result);
+            };
+            fileReader.onerror = (e) => {
+                reject(e);
+            };
+            fileReader.readAsDataURL(file);
+        })
     }
     getValues(){
         let user = {};
